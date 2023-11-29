d2978b13c2318089697cb86acdc3e48ecb8ddf26
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-11-29 11:38:22 -0600

    patron numeros

---
+++ patron_numerico.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int n = 5;
+    int filas = 0;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=(n-i)+1; j++){
+
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+        filas++;
+    }
+
+
+
+}
